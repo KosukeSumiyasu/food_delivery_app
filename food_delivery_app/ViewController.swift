58ad246d247992c2e29be76a0b17d36e3a926a9c
KousukeSumiyasu <kosuke.sumiyasu@gmail.com>
2021-01-15 15:53:34 +0900

    Initial Commit

---
+++ food_delivery_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  food_delivery_app
+//
+//  Created by Kousuke Sumiyasu on 2021/01/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
